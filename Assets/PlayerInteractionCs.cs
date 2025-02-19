1f6f7ab04445d8688ead50698f8ab826f7057cee
annikepo <anni.kepczynski@hyperisland.se>
2025-02-19 22:22:06 +0100

    Testing Player Interaction

--- Assets/PlayerInteractionCs.cs
+++ Assets/PlayerInteractionCs.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
+    public Transform playerHand; // Reference to the camera rig's hand transform
+    public float grabSpeed = 5f; // Speed at which the flower will move to the hand
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Flower"))
